19705116fd8b178ebd1327e2bb821c19f4bb2281
Joseph Gonzalez <joseph15809@gmail.com>
2025-05-17 18:45:27 -0700

    initial repo

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi.responses import Response, HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+def read_html(file_path: str) -> str:
+    with open(file_path, "r") as f:
+        return f.read() 
+
+@app.get("/", response_class=HTMLResponse)
+def home_html():
+    return HTMLResponse(content=read_html("app/static/homepage.html"))
