f257a65b139641b270fa8ff8f93eaa349f44b269
Joseph Gonzalez <joseph15809@gmail.com>
2025-05-29 14:31:22 -0700

    updated team section

--- app/static/js/team.js
+++ app/static/js/team.js
@@ -6,18 +6,26 @@ const joe = document.getElementById("joe-section");
 
 const clone1 = tpl.content.cloneNode(true);
 clone1.querySelector(".card-fullname").textContent = "Akhil Kalakota";
+clone1.querySelector(".card-jobtitle").textContent = "Frontend Developer";
+clone1.querySelector(".card-desc").textContent = "Zoe main in league";
 akhil.appendChild(clone1);
 
 const clone2 = tpl.content.cloneNode(true);
 clone2.querySelector(".card-fullname").textContent = "Ohm R Mahmood";
+clone2.querySelector(".card-jobtitle").textContent = "Game Developer";
+clone2.querySelector(".card-desc").textContent = "Loves Minecraft but doesn't want to develop Minecraft 2";
 omar.appendChild(clone2);
 
 const clone3 = tpl.content.cloneNode(true);
 clone3.querySelector(".card-fullname").textContent = "Brian Huynh";
+clone3.querySelector(".card-jobtitle").textContent = "Game Designer/Lore";
+clone3.querySelector(".card-desc").textContent = "Big Data";
 brian.appendChild(clone3);
 
 const clone4 = tpl.content.cloneNode(true);
 clone4.querySelector(".card-fullname").textContent = "Joseph Gonzalez";
+clone4.querySelector(".card-jobtitle").textContent = "Fullstack Developer";
+clone4.querySelector(".card-desc").textContent = "Doomfist one trick";
 joe.appendChild(clone4);
 
 
